09b9f6cc3f52e2ddee81a1d8062b389e6ead991a
soldrag <soldrag@gmail.com>
2020-03-27 14:37:41 +0300

    start developing server and client

---
+++ server.py
@@ -0,0 +1,37 @@
+import asyncio
+
+
+async def receiver():
+    pass
+
+
+def run_server(host: str, port: int):
+    class ClientServerProtocol(asyncio.Protocol):
+
+        def connection_made(self, transport) -> None:
+            self.transport = transport
+
+        def data_received(self, data: bytes) -> None:
+            resp = data.decode()
+            self.transport.write(b"hello")
+
+
+    def resonse_handler(data):
+        data.decode()
+        
+
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(ClientServerProtocol, str(host), port)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+if __name__ == "__main__":
+    run_server("127.0.0.1", 8787)
